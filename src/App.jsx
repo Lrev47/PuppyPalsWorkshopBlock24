542d7e49d1a648601831d4a4e84875dbdaa67b20
Lrev47 <luis.revilla47@yahoo.com>
2023-12-13 23:52:03 -0600

    rendered_Names

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,11 @@ function App() {
   return (
     <>
       <div>
+        {
+          puppies.map((puppy) =>{
+            return <p>{puppy.name}</p>
+          })
+        }
       </div>
     </>
   )
