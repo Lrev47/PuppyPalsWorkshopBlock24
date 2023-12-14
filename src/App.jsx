dcb8532871480947b18215550f19d15a4c8592f2
Lrev47 <luis.revilla47@yahoo.com>
2023-12-14 02:22:35 -0600

    addingCSS

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,28 @@ function App() {
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null)
   console.log("Puppy List:", puppies)
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+  console.log(featuredPup)
 
-  return (
+  return (<div>
+    <h1>Puppy Pals!</h1>
+    <div id="verticalView">
       <div className="App">
-        {puppies.map((puppy) =>(
-            <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+        {puppies.map((puppy) => (
+          <p onClick={() => setFeatPupId(puppy.id)} key={puppy.id}>{puppy.name}</p>
         ))}
-          { featPupId && <p>{ featPupId }</p> } 
       </div>
+      {featPupId && (
+        <div className="pupCard">
+          <h2>{featuredPup ? featuredPup.name : ''}</h2>
+          <ul>
+            <li>Age: {featuredPup ? featuredPup.age : ''}</li>
+            <li>Email: {featuredPup ? featuredPup.email : ''}</li>
+          </ul>
+        </div>
+      )}
+    </div>
+    </div>
   );
 }
 
